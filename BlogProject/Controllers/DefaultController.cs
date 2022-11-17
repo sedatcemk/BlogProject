c391e9283cf07d02d632b83ffed50aeb7c6fb62a
sedatcemk <kiziltoprak.cem@gmail.com>
2022-11-18 00:48:35 +0100

    initial

---
+++ BlogProject/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogProject.Models.Class;
+using BlogProject.Models.Entity;
+
+namespace BlogProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        BlogDbEntities db = new BlogDbEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Value1 = db.TBL_About.ToList();
+            cs.Value2 = db.TBL_Contact.ToList();
+            var values = db.TBL_About.ToList();
+            return View(cs);
+        }
+    }
+}
---
+++ BlogProject/Models/Class/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BlogProject.Models.Entity;
+
+namespace BlogProject.Models.Class
+{
+    public class Class1
+    {
+        public IEnumerable<TBL_About> Value1 { get; set; }
+        public IEnumerable<TBL_Contact> Value2 { get; set; }
+
+    }
+}
